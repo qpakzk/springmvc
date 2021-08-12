61d67189f935c840d83a067f183521eff046dccc
Sangwon Hong <qpakzk@gmail.com>
2021-08-13 00:10:48 +0900

    [섹션 7] PRG Post/Redirect/Get

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -70,12 +70,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
 
         itemRepository.save(item);
 
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
